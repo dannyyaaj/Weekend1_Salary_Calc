ad63228839308de583924756b05b97fc96abf6ef
Danny Yang <dannyyaaj@gmail.com>
2018-06-22 16:13:48 -0500

    added initial employee array

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-$(document).ready(start)
+$(document).ready(start);
+
+// initalize array of employees
+const employees = [{
+  employeeFirstName: 'Bruno',
+  employeeLastName: 'Mars',
+  employeeTitle: 'Manager',
+  employeeSalary: 78000
+}, {
+  employeeFirstName: 'Mary',
+  employeeLastName: 'Jane',
+  employeeTitle: 'Support Team',
+  employeeSalary: 65000
+}, {
+  employeeFirstName: 'Jingbo',
+  employeeLastName: 'Lin',
+  employeeTitle: 'Recruiter',
+  employeeSalary: 56000
+}];
 
 function start() {
-  console.log('jQuery is ready!')
+  console.log('jQuery ready!')
 }
