3a6db6a4146297ae4558ab1ccb7fa797116321c5
Danny Yang <dannyyaaj@gmail.com>
2018-06-24 23:28:10 -0500

    created Employee class

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(start);
 
+
+class Employee {
+  constructor(fName, lName, id, title, salary) {
+    this.employeeFirstName = fName;
+    this.employeeLastName = lName;
+    this.employeeId = id;
+    this.employeeTitle = title;
+    this.employeeSalary = salary;
+  }
+}
+
 // initalize array of employees
 const employees = [{
   employeeFirstName: 'Bruno',
@@ -41,14 +52,6 @@ function start() {
   }
 }
 
-function renderTotalMonthly() {
-  let totalMonthly = 0;
-  for (let i = 0; i < employees.length; i++) {
-    totalMonthly += (employees[i].employeeSalary) / 12
-  }
-  $('#totalMonthly').text(`$${totalMonthly.toLocaleString('en')}`);
-}
-
 function handleEvents() {
   $('#submit').on('click', handleSubmit)
 }
@@ -59,12 +62,35 @@ function handleSubmit() {
   let lName = $('#employeeLastName').val();
   let id = $('#employeeId').val();
   let title = $('#employeeTitle').val();
-  let salary = $('#employeeSalary').val();
+  let salary = parseInt($('#employeeSalary').val());
+
+  let newEmployee = new Employee(fName, lName, id, title, salary);
 
   $newRow.append(`<td>${fName}</td>`);
   $newRow.append(`<td>${lName}</td>`);
   $newRow.append(`<td>${id}</td>`);
   $newRow.append(`<td>${title}</td>`);
-  $newRow.append(`<td>$${salary}</td>`);
+  $newRow.append(`<td>$${salary.toLocaleString('en')}</td>`);
   $('#employeeTable').append($newRow);
+
+  employees.push(newEmployee);
+  renderTotalMonthly();
+}
+
+function renderTotalMonthly() {
+  let totalMonthly = 0;
+  for (let i = 0; i < employees.length; i++) {
+    totalMonthly += (employees[i].employeeSalary) / 12
+  } // end of looping through employees object
+
+  // Changes total monthly to red if amount is over $15,000
+  totalMonthly >= 15000 ? $('#totalMonthly').toggleClass('fontRed') : null;
+
+  // Rounds total monthly salary and adds comma to number
+  let newTotalMonthly = Math.round(totalMonthly).toLocaleString('en');
+
+ // Insert total monthly salary into span
+  $('#totalMonthly').text(`$${newTotalMonthly}`);
+
+
 }
