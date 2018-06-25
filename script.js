ab2aff64c7749d814edc257f906f57230fdf6ab6
Danny Yang <dannyyaaj@gmail.com>
2018-06-24 22:05:40 -0500

    correctly displays total monthly salary

--- script.js
+++ script.js
@@ -4,21 +4,29 @@ $(document).ready(start);
 const employees = [{
   employeeFirstName: 'Bruno',
   employeeLastName: 'Mars',
+  employeeId: 4521,
   employeeTitle: 'Manager',
   employeeSalary: 73000
 }, {
   employeeFirstName: 'Mary',
   employeeLastName: 'Jane',
+  employeeId: 8724,
   employeeTitle: 'Support Team',
   employeeSalary: 64000
 }, {
   employeeFirstName: 'Jingbo',
   employeeLastName: 'Lin',
+  employeeId: 9623,
   employeeTitle: 'Recruiter',
   employeeSalary: 55000
 }];
 
+
+
 function start() {
+  renderTotalMonthly()
+  handleEvents()
+
   console.log('jQuery ready!');
   // renderEmployeeData()
 
@@ -26,9 +34,37 @@ function start() {
     let $newRow = $('<tr></tr>');
     $newRow.append(`<td>${employee.employeeFirstName}</td>`);
     $newRow.append(`<td>${employee.employeeLastName}</td>`);
-    $newRow.append(`<td>${employee.employeeFirstId}</td>`);
+    $newRow.append(`<td>${employee.employeeId}</td>`);
     $newRow.append(`<td>${employee.employeeTitle}</td>`);
-    $newRow.append(`<td>$${employee.employeeSalary}</td>`);
+    $newRow.append(`<td>$${employee.employeeSalary.toLocaleString('en')}</td>`);
     $('#employeeTable').append($newRow);
   }
+}
+
+function renderTotalMonthly() {
+  let totalMonthly = 0;
+  for (let i = 0; i < employees.length; i++) {
+    totalMonthly += (employees[i].employeeSalary) / 12
+  }
+  $('#totalMonthly').text(`$${totalMonthly.toLocaleString('en')}`);
+}
+
+function handleEvents() {
+  $('#submit').on('click', handleSubmit)
+}
+
+function handleSubmit() {
+  let $newRow = $('<tr></tr>');
+  let fName = $('#employeeFirstName').val();
+  let lName = $('#employeeLastName').val();
+  let id = $('#employeeId').val();
+  let title = $('#employeeTitle').val();
+  let salary = $('#employeeSalary').val();
+
+  $newRow.append(`<td>${fName}</td>`);
+  $newRow.append(`<td>${lName}</td>`);
+  $newRow.append(`<td>${id}</td>`);
+  $newRow.append(`<td>${title}</td>`);
+  $newRow.append(`<td>$${salary}</td>`);
+  $('#employeeTable').append($newRow);
 }
