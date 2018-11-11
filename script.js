d2f5ab31dafabc3815b31bac63c0922be2992182
Danny Yang <dannyyaaj@gmail.com>
2018-11-10 20:40:59 -0600

    updated salary calculator functionality

--- script.js
+++ script.js
@@ -9,45 +9,43 @@ class Employee {
     this.employeeSalary = salary;
   }
 }
-// initalize array of employees
+// initialize array of employees
 const employees = [{
   employeeFirstName: 'Bruno',
   employeeLastName: 'Mars',
   employeeId: 4521,
-  employeeTitle: 'Manager',
-  employeeSalary: 65000
+  employeeTitle: 'Technical Project Manager',
+  employeeSalary: 75000
 }, {
   employeeFirstName: 'Mary',
   employeeLastName: 'Jane',
   employeeId: 8724,
-  employeeTitle: 'Support Team',
-  employeeSalary: 53000
+  employeeTitle: 'Web Developer',
+  employeeSalary: 59000
 }, {
   employeeFirstName: 'Jingbo',
   employeeLastName: 'Lin',
   employeeId: 9623,
-  employeeTitle: 'Recruiter',
-  employeeSalary: 48000
+  employeeTitle: 'Graphic Designer',
+  employeeSalary: 55000
 }];
 
 let totalMonthly = 0;
 
-// jQuery will run this function start on load
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
 
+// jQuery will run this function start on load
 function start() {
   // renderEmployeeData()
   for (let employee of employees) {
     addRow(employee.employeeFirstName, employee.employeeLastName, employee.employeeId, employee.employeeTitle, employee.employeeSalary);
   };
-
   handleEvents();
   renderTotalMonthly();
-}
-
-function handleEvents() {
-  $('#submit').on('click', handleSubmit);
-  $('#submit').on('click', updateTotalMonthly);
-  $('#employeeTable').on('click', '.deleteBtn', handleDeleteButton);
 }
 
 function handleSubmit() {
@@ -57,7 +55,6 @@ function handleSubmit() {
   let id = $('#employeeId').val();
   let title = $('#employeeTitle').val();
   let salary = parseInt(`${$(('#employeeSalary').toLocaleString('en')).val()}`);
-
 
   // initialize new employee object from input fields and push it into array
   let newEmployee = new Employee(fName, lName, id, title, salary);
@@ -80,19 +77,18 @@ function handleSubmit() {
   $('#employeeSalary').val('');
 }
 
-// function add rows of new eployees
+// function add rows of new employees
 function addRow(fName, lName, id, title, salary) {
   let $newRow = $('<tr></tr>');
-
   // add new table row and display input fields as values
   $newRow.append(`<td>${fName}</td>`);
   $newRow.append(`<td>${lName}</td>`);
   $newRow.append(`<td>${id}</td>`);
   $newRow.append(`<td>${title}</td>`);
-  $newRow.append(`<td>$${salary.toLocaleString('en')}</td>`);
+  $newRow.append(`<td>${(currencyFormatter.format(salary))}</td>`);
+  $newRow.append(`<td>${(currencyFormatter.format(salary / 12))}</td>`)
   $newRow.append(`<td><button class='deleteBtn' data-id=${id}>Delete</button></td>`);
   $('#employeeTable').append($newRow);
-  // renderTotalMonthly();
 }
 
 function renderTotalMonthly() {
@@ -104,27 +100,38 @@ function renderTotalMonthly() {
   $('#totalMonthly').text(`$${totalMonthly.toFixed(2)}`)
 }
 
-// update total monthly salary
+function handleEvents() {
+  $('#submit').on('click', handleSubmit);
+  $('#submit').on('click', updateTotalMonthly);
+  $('#employeeTable').on('click', '.deleteBtn', handleDeleteButton);
+}
+
 function updateTotalMonthly() {
+  for (let i = 0; i < employees.length; i++) {
+    totalMonthly += ((employees[i].employeeSalary) / 12);
+  }
 
-  totalMonthly += (employees[employees.length - 1].employeeSalary) / 12
-  // Changes total monthly to red if amount is over $15,000
+  // Changes total monthly to red if amount is over $20,000
+  totalMonthly >= 20000 ? $('#totalMonthly').toggleClass('fontRed') : $('#totalMonthly').removeClass('fontRed');
 
-  totalMonthly >= 20000 ? $('#totalMonthly').toggleClass('fontRed') : null;
-
-  // Rounds total monthly salary and adds comma to number
-  let newTotalMonthly = Math.round(totalMonthly);
-
-  // Insert total monthly salary into span
-  $('#totalMonthly').text(`$${newTotalMonthly.toFixed(2)}`);
+  // end of looping through employees object
+  $('#totalMonthly').text(`$${totalMonthly.toFixed(2)}`)
 }
 
 function handleDeleteButton() {
+  // remove closes table row to delete button
   $(this).closest('tr').remove();
+
+  //loop through employees array
   for (let i = 0; i < employees.length; i++) {
     let employee = employees[i];
     if (employee.employeeId == $(this).data().id) {
+      console.log(employees, 'before');
       employees.splice(i, 1);
+      console.log(employees, 'after');
+      console.log(totalMonthly, 'totalMonthly')
+      totalMonthly = 0;
+      updateTotalMonthly()
     }
   }
 }
